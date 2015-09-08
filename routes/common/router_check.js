10433959b1da90b07e135124300bbc17103c9acd
胡鹏 <hupenghp2005@gmail.com>
2015-09-08 22:48:48 +0800

    modify access control

--- routes/common/router_check.js
+++ routes/common/router_check.js
@@ -4,6 +4,10 @@ var path = require('path');
 
 var queryCenter = require(path.join(global.rootPath, 'routes/common/common'));
 var msg = require(path.join(global.rootPath,'define/msg')).global_msg_define;
+var redis_mgr = require(path.join(global.rootPath,'redis/redis_mgr'));
+var redis_define = require(path.join(global.rootPath, 'define/redis')).redis_define;
+
+var _ = {};
 
 router.use(function(req, res, next){
     var queryObj = queryCenter.get(req);
@@ -17,8 +21,18 @@ router.use(function(req, res, next){
 		iPhoneNum = req.cookies.parking_app_key.split('_')[0];
 	    }catch(e){
 	    }
-	    req.url += '&iPhoneNum=' + iPhoneNum;
-	    next();
+	    redis_mgr.get2(redis_define.enum.LOGIN, iPhoneNum, function(err, user){
+		if(err || !user){
+		    msg.wrapper(msg.code.ERR_ACCESS_FAIL, null, res);	
+		}else{
+		    if(user.parking_app_key == req.cookies.parking_app_key){
+			req.url += '&iPhoneNum=' + iPhoneNum;
+			next();
+		    }else{
+			msg.wrapper(msg.code.ERR_ACCESS_FAIL, null, res);	
+		    }
+		}
+	    });
 	}else{
 	    msg.wrapper(msg.code.ERR_ACCESS_FAIL, null, res);	
 	}
