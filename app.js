6054782838eb3a3b2d6577e57c4602e88e168eba
胡鹏 <hupenghp2005@gmail.com>
2015-10-05 18:58:04 +0800

    modify logger problem

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var logger = require(path.join(global.rootPath,'util/logger')).logger;
 var console_trace = require('debug-trace')({always:true});
 
 var app = express();
 
-var logger = require(path.join(global.rootPath,'util/logger')).logger;
 var redis_mgr = require(path.join(global.rootPath,'redis/redis_mgr'));
 var queryCenter = require(path.join(global.rootPath, 'routes/common/common'));
 var aliyunBiz = require(path.join(global.rootPath, 'interfaceBiz/aliyunBiz.js'));
